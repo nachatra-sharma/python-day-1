96027f291e1661600c5528436aa259a80cba4ea1
nakshCoder <nakshusharm0@gmail.com>
2023-09-29 09:55:53 +0530

    Day - 1 Printing, Input, Variable, Comment

--- Day_1.py
+++ Day_1.py
@@ -1,11 +1,11 @@
-# Exercise 1
+# Exercise 1 - How to print something in python
 '''
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
 '''
 
-# Exercise 2
+# Exercise 2 - String Concatenation
 '''
 print("Day 1 - String Manipulation")
 print("String Concatenation is done with the " + '"+"' + " sign.")
@@ -13,12 +13,12 @@ print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
 '''
 
-# Exercise 3
+# Exercise 3 - Length of the input name 
 '''
 print(len(input("What is your name? ")))
 '''
 
-# Exercise 4
+# Exercise 4 - Swapping two numbers
 '''
 a = input("a: ")
 b = input("b: ")
@@ -29,7 +29,7 @@ print("The value of a is: " + a)
 print("The value of b is: " + b)
 '''
 
-# Day 1 Project
+# Day 1 Project - Band name genrator
 '''
 print("Welcome to the band name genrator.")
 city_name = input("What's name of the city you grew up in?")
