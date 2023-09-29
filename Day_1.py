bcbd181bc8a09f8af3f5a36b20f807aa79c21649
nakshCoder <nakshusharm0@gmail.com>
2023-09-29 09:51:08 +0530

    Day 1 - Printing, Input, Comment, Variable

---
+++ Day_1.py
@@ -0,0 +1,38 @@
+# Exercise 1
+'''
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+'''
+
+# Exercise 2
+'''
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the " + '"+"' + " sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+'''
+
+# Exercise 3
+'''
+print(len(input("What is your name? ")))
+'''
+
+# Exercise 4
+'''
+a = input("a: ")
+b = input("b: ")
+c = a
+a = b
+b = c
+print("The value of a is: " + a)
+print("The value of b is: " + b)
+'''
+
+# Day 1 Project
+'''
+print("Welcome to the band name genrator.")
+city_name = input("What's name of the city you grew up in?")
+pet_name = input("What's your pet name?")
+print(f"Your band name could be {city_name} {pet_name}")
+'''
